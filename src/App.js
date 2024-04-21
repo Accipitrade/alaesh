4449759260e5e6f5e0f8c8c5ccc9da9876fe5a47
Josh Slavin <47463508+Accipitrade@users.noreply.github.com>
2024-04-21 02:29:34 -0400

    project start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className='background' >
+        <img src={logo} alt="Logo" className="logo" />
+        <h4 className="subtitle">Traditional Southern Barbecue Techniques. Traditional Middle Eastern Spices.</h4>
+        <h1 style={{textAlign:'center'}}>View Menu by Location</h1>
+        <div className="buttons">
+          <button>View Sharon Menu</button>
+          <span>368 S Main St, Sharon, MA</span>
+          <button>View Normandy Farms Menu</button>
+          <button>Order Online</button>
+          <span>72 W Foxborough St, Foxborough, MA</span>
+        </div>
       </header>
+      <section className="about-us">
+        <h2>About Us</h2>
+        <p>Our team, led by Executive Chef Avi Shemtov, approaches barbecue from a unique perspective. Focused on award winning barbeque quality and progressive flavors, a La Esh is something very familiar that's unlike anything you've ever had before.</p>
+        <p>In recent years, a La Esh has partnered with luxury campground Normandy Farms in Foxborough, Massachusetts, to bring Chef Avi's unique take on barbecue to the campground. Join us in the spring, summer and fall inside Normandy farms to try our unique take on barbecue and classic camp favorites!</p>
+      </section>
+      <section className="gallery">
+        {/* Gallery content goes here */}
+      </section>
+      <section className="inquiry">
+        <form>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </section>
     </div>
   );
 }
